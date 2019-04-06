029ba6a541cb1ff31f359e2f07cdf6df656ea3f9
Carlostheboss <cfpc141@gmail.com>
2019-04-06 21:36:37 +0100

    MENU

---
+++ DoggoPlease/Assets/NextScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+  
+
+
+        public void GotoMainScene()
+        {
+        Invoke("LoadScene", 0.25f);        
+        }
+
+        public void LoadScene()
+        {
+        SceneManager.LoadScene("DiogoScene");
+        }
+    
+}
