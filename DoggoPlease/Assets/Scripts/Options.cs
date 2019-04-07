fe34277f82b79e74775af84491aa33d330dfe88e
Ricardo-Cacheira <34067691+Ricardo-Cacheira@users.noreply.github.com>
2019-04-07 12:08:30 +0100

    Options and FMOD logo

---
+++ DoggoPlease/Assets/Scripts/Options.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public bool on = false;
+    public GameObject panel;
+    public SoundManager soundManager;
+    public Slider st;
+    public Slider sfx;
+
+    void Start()
+    {
+        st.value = soundManager.volume1;
+        sfx.value = soundManager.volume3;
+    }
+
+    public void Toggle()
+    {
+        on = !on;
+        panel.SetActive(on);
+    }
+
+    public void UpdateValues()
+    {
+        soundManager.volume1 = (int)st.value;
+        soundManager.volume2 = (int)st.value;
+        soundManager.volume3 = (int)sfx.value;
+    }
+}
